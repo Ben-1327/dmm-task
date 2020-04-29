a57b2db3e7eb169d9835444f9b1cddec7a3e42f6
Ben-1327 <ff.walker1327@gmail.com>
2020-04-29 17:21:04 +0900

    [Finish] 6-7 [Start] 6-8

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -14,11 +14,38 @@ $(function(){
     //   $('.box1').addClass('box1-ext');
     // });
 
-    $('.box1').on('click', function(){
-      $('.box1').addClass('box1-ext');
-    });
+    // $('.box1').on('click', function(){
+    //   $('.box1').addClass('box1-ext');
+    // });
+    //
+    // $('.box1').mouseout(function(){
+    //   $('.box1').removeClass('box1-ext');
+    // });
 
-    $('.box1').mouseout(function(){
-      $('.box1').removeClass('box1-ext');
+
+
+// 6-7
+    // $('.bg1').on('click', function(){
+    //   $('.bg1').slideUp();
+    // });
+    //
+    // $('.bg2').on('click', function(){
+    //   $('.bg2').slideUp();
+    // });
+    //
+    // $('.bg3').on('click', function(){
+    //   $('.bg3').slideUp();
+    // });
+    //
+    // $('.bg4').on('click', function(){
+    //   $('.bg4').slideUp();
+    // });
+
+    // $('.box1').on('click', function(){
+    //   $(this).slideUp();
+    // });
+
+    $('button').on('click', function(){
+      $('ul').children().css('color', 'red');
     });
 });
