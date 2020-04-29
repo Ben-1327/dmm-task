8a09da025489a5d00d28fc470efa92dec73ccaf9
Ben-1327 <ff.walker1327@gmail.com>
2020-04-29 15:04:42 +0900

    [Finish] 6-6 [Start] 6-7

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,9 +1,24 @@
 $(function(){
-    $('.box1').slideDown(function(){
-      $('.box1').css({
-        'background-color': '#0000ff',
-        'width': '200px',
-        'height' : '100px'
-      }).slideUp();
+
+// 6-5
+    // $('.box1').slideDown(function(){
+    //   $('.box1').css({
+    //     'background-color': '#0000ff',
+    //     'width': '200px',
+    //     'height' : '100px'
+    //   }).slideUp();
+    // });
+
+// 6-6
+    // $('.box1').mouseover(function(){
+    //   $('.box1').addClass('box1-ext');
+    // });
+
+    $('.box1').on('click', function(){
+      $('.box1').addClass('box1-ext');
+    });
+
+    $('.box1').mouseout(function(){
+      $('.box1').removeClass('box1-ext');
     });
 });
